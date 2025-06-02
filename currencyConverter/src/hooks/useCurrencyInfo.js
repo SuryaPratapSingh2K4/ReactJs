82ee7cf52a7ea9123f527ea7b5111a9c63a59b52
surya pratap singh <surya2pratap0singh04@gmail.com>
2025-06-02 22:34:26 +0530

    currency commit

---
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,41 @@
+// import { useEffect, useState } from "react";
+
+// const useCurrencyInfo = (currency) => {
+//     const [data, setData] = useState({});
+
+//     useEffect(() => {
+//         if (!currency) return;
+//         const fetchData = async () => {
+//             try {
+//                 const res = await fetch(
+//                     `https://api.exchangerate-api.com/v4/latest/${currency}`
+//                 );
+//                 const json = await res.json();
+//                 setData(json.rates || {});
+//             } catch (error) {
+//                 setData({});
+//             }
+//         };
+//         fetchData();
+//     }, [currency]);
+
+//     return data;
+// };
+
+// export default useCurrencyInfo;
+
+import { useEffect,useState } from "react";
+
+function useCurrencyInfo(currency){
+    const [data,setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0
+            /currency-api@latest/v1/currencies
+            /${currency}.json`).then((res) => res.json())
+            .then((res) => setData(res[currency]))
+    },[currency])
+    console.log(data);
+    return data
+}
+
+export default useCurrencyInfo;
