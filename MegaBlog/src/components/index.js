e2012507bc1919ccdf8fe6a51209039322da6f73
surya pratap singh <surya2pratap0singh04@gmail.com>
2025-06-15 19:16:45 +0530

    login

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -3,5 +3,7 @@ import Header from "./Header/Header";
 import Container from "./container/container.jsx";
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
+import Input from "./Input.jsx";
+import Button from "./Button.jsx";
 
-export { Footer, Header, Container, Logo, LogoutBtn };
+export { Footer, Header, Container, Logo, LogoutBtn, Input, Button };
