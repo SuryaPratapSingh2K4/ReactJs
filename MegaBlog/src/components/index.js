95cae747c24bb48b8a08fd2974eee8d41c90c551
surya pratap singh <surya2pratap0singh04@gmail.com>
2025-06-17 22:38:17 +0530

    Postform

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -5,5 +5,7 @@ import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
 import Input from "./Input.jsx";
 import Button from "./Button.jsx";
+import RTE from "./RTE.jsx";
 
-export { Footer, Header, Container, Logo, LogoutBtn, Input, Button };
+
+export { Footer, Header, Container, Logo, LogoutBtn, Input, Button, RTE };
